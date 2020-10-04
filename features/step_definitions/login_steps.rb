00ab10da6a66c7ff50a786720de4376b2029d360
Renato Reis <renatoreis@live.com>
2020-10-04 17:03:45 -0400

    Finalizando a primeira step da login.feature.

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,18 @@
+Quando('eu faço login com {string} e {string}') do |email, password|
+    visit "/"
+    find("#emailId").set email
+    find("#passId").set password
+    click_button "Entrar"
+    sleep 5
+  end
+  
+  Então('devo ser autenticado') do
+    js_script = 'return window.localStorage.getItem("default_auth_token");'
+    token = page.execute_script(js_script)
+    expect(token.length).to be 147
+  end
+  
+  Então('devo ver {string} a área logada') do |expect_name|
+    user = find('.sidebar-wrapper .user .info span')
+    expect(user.text).to eql expect_name
+  end
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,6 @@ require "capybara/cucumber"
 require "selenium-webdriver"
 
 Capybara.configure do |config|
-    config.default_driver = :selenium
-    config.app_host = "http://localhost:8080/"
+    config.default_driver = :selenium_chrome
+    config.app_host = "http://localhost:8080"
 end
